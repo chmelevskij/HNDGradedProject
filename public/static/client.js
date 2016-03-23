8c47a3ff8324a7f8ba3fee6958fda4078c3fbcf6
Tomas Chmelevskij <T.Chmelevskij@gmail.com>
2016-03-23 05:12:39 +0000

    added movement

--- public/static/client.js
+++ public/static/client.js
@@ -1,31 +1,54 @@
-var Paddle = function (){};
-Paddle.constructor = Paddle;
-
-function init(){
-	var canvas = window.getElementById('cnv'),
-		context = canvas.getContext('2d');
-
-	var host = window.location.host,
-		wsURL = "ws://" + host + "/ws";
-		ws = new WebSocket(wsURL);
-
-	ws.onopen = function (e){
-		console.log("Websocket open on: " + wsURL);
-	}
-
-	ws.onclose = function (e){
-		console.log("WebSocket was closed");
-	}
-
-	ws.onmessage = function (e){
-		console.log("Received message from server:\n" + e.data);
-		context.rect(20,20,150,100);
-		context.stroke();
-	}
-
-	ws.onerror = function (e){
-		console.log("Something went wrong with the connection");
-	}
+// Utilities
+var logger = document.getElementById('content');
+function log(message) {
+    logger.value += message + '\n';
 }
-document.location
-window.addEventListener('load', init, false);
+
+// Classes
+var Paddle = function () {
+};
+Paddle.constructor = Paddle;
+Paddle.prototype = {
+    color: 'rgb(0,0,0)',
+    x: 10,
+    y: 10,
+    width: 20,
+    height: 100,
+    alpha: 1
+}
+
+// Rendering
+var canvas = document.getElementById('cnv'),
+context = canvas.getContext('2d'),
+paddle = new Paddle();
+
+function render() {
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    context.fillStyle = paddle.color;
+    if (paddle.y < 0 || isNaN(paddle.y)) {
+        paddle.y = 0;
+    } else if  (paddle.y > canvas.height|| isNaN(paddle.y)){
+        paddle.y = canvas.height;
+    }
+    context.fillRect(paddle.x, paddle.y, paddle.width, paddle.height)
+    window.requestAnimationFrame(render);
+}
+render();
+
+// Connection
+var host = window.location.host,
+wsURL = 'ws://' + host + '/ws';
+ws = new WebSocket(wsURL);
+ws.onopen = function (e) {
+    console.log('Websocket open on: ' + wsURL);
+}
+ws.onclose = function (e) {
+    console.log('WebSocket was closed');
+}
+ws.onmessage = function (e) {
+    console.log('Received message from server:' + e.data);
+    paddle.y += parseInt(e.data);
+}
+ws.onerror = function (e) {
+    console.log('Something went wrong with the connection');
+}
